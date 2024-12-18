c17d1915ebc00b8dcd6e54699c97c5efdbf19854
yeabnoah <yeabnoah5@gmail.com>
2024-12-18 10:37:49 +0300

    https://frame.nerdspacer.com/api/auth

--- client2/lib/auth-client.ts
+++ client2/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 const authClient = createAuthClient({
-  baseURL: "https://frame.nerdspacer.com",
+  baseURL: "https://frame.nerdspacer.com/api/auth",
 });
 
 export default authClient;
