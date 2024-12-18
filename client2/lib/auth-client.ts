b9045b7fd8301b1bbccd77742f1f53021c4e9ba0
yeabnoah <yeabnoah5@gmail.com>
2024-12-18 12:01:23 +0300

    https://frame.nerdspacer.com/api/auth

--- client2/lib/auth-client.ts
+++ client2/lib/auth-client.ts
@@ -1,8 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
 const authClient = createAuthClient({
-  // baseURL: "https://frame.nerdspacer.com",
-  baseURL: "https://dev-portifolio-backend.vercel.app/api/auth",
+  baseURL: "https://frame.nerdspacer.com",
+  // baseURL: "https://dev-portifolio-backend.vercel.app/api/auth",
 });
 
 export default authClient;
