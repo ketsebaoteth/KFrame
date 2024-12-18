e191e7d47da47f3614002600809d8139a8baab26
yeabnoah <yeabnoah5@gmail.com>
2024-12-18 12:14:44 +0300

    cookie fixed and also base url --- all inside of the auth-client file

--- client2/lib/auth-client.ts
+++ client2/lib/auth-client.ts
@@ -1,6 +1,14 @@
 import { createAuthClient } from "better-auth/react";
 
 const authClient = createAuthClient({
+  advanced: {
+    cookies: {
+      sessionToken: {
+        sameSite: "none",
+        secure: true,
+      },
+    },
+  },
   baseURL: "https://frame.nerdspacer.com/api/auth",
   // baseURL: "https://dev-portifolio-backend.vercel.app/api/auth",
 });
