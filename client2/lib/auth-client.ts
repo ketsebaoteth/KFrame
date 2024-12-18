67551d82af62d057af72e3dca09a5bad1a6c6268
yeabnoah <yeabnoah5@gmail.com>
2024-12-18 14:07:59 +0300

    cookie fixed and also base url --- all inside of the auth-client file

--- client2/lib/auth-client.ts
+++ client2/lib/auth-client.ts
@@ -2,15 +2,15 @@ import { createAuthClient } from "better-auth/react";
 
 const authClient = createAuthClient({
   baseURL: "https://porify.vercel.app/api/auth",
-  advanced: {
-    crossSubDomainCookies: {
-      enabled: true,
-    },
-    defaultCookieAttributes: {
-      sameSite: "none",
-      secure: true,
-    },
-  },
+  // advanced: {
+  //   crossSubDomainCookies: {
+  //     enabled: true,
+  //   },
+  //   defaultCookieAttributes: {
+  //     sameSite: "none",
+  //     secure: true,
+  //   },
+  // },
 });
 
 export default authClient;
