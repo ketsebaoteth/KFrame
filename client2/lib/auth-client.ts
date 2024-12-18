1c9e7f304fc2a60cb804948e35e5c7876034f285
yeabnoah <yeabnoah5@gmail.com>
2024-12-18 13:34:50 +0300

    cookie fixed and also base url --- all inside of the auth-client file

--- client2/lib/auth-client.ts
+++ client2/lib/auth-client.ts
@@ -1,14 +1,13 @@
 import { createAuthClient } from "better-auth/react";
 
 const authClient = createAuthClient({
-  baseURL: "https://frame.nerdspacer.com/api/auth",
+  baseURL: "https://porify.vercel.app/api/auth",
   advanced: {
     defaultCookieAttributes: {
       sameSite: "none",
       secure: true,
     },
   },
-  // baseURL: "https://dev-portifolio-backend.vercel.app/api/auth",
 });
 
 export default authClient;
