66324dd08cea5eecb14068446a8d21ec626cf5ab
Bereket Engida <bekacru@gmail.com>
2024-12-21 13:54:25 +0300

    fix: potentially fix for cookies

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,32 +1,3 @@
-// import { betterAuth } from "better-auth";
-// import { prismaAdapter } from "better-auth/adapters/prisma";
-// import { PrismaClient } from "@prisma/client";
-// import prisma from "./db";
-
-// export const auth = betterAuth({
-//   database: prismaAdapter(prisma, {
-//     provider: "postgresql",
-//   }),
-//   emailAndPassword: {
-//     enabled: true,
-//   },
-//   socialProviders: {
-//     github: {
-//       clientId: process.env.GITHUB_CLIENT_ID as string,
-//       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-//     },
-//     google: {
-//       clientId: process.env.GOOGLE_CLIENT_ID as string,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-//     },
-//   },
-//   trustedOrigins: [
-//     "http://localhost:3000",
-//     "https://frame.nerdspacer.com",
-//     "https://frame.nerdspacer.com/auth",
-//     "http://localhost:3000/auth",
-//   ],
-// });
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
@@ -61,10 +32,10 @@ export const auth = betterAuth({
     "http://localhost:3001/auth",
   ],
   advanced: {
-    defaultCookieAttributes: {
-      sameSite: "none",
-      secure: true,
-    },
+    crossSubDomainCookies: {
+      enabled: process.env.NODE_ENV === "production",
+      domain: process.env.NODE_ENV === "production" ? ".nerdspacer.com" : "",
+    }
   },
   // advanced: {
   //   defaultCookieAttributes: {
